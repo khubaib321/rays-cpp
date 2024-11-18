70db44ab9cf3b5c8638d2ea2d8cf6069a91d83c2
Khubaib Afzal <khubaib.afzal11@gmail.com>
2024-11-18 01:50:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Remove old executable if it exists
 rm -f rays
 
-# Compile the program
 g++ -std=c++14 -I/opt/homebrew/include -L/opt/homebrew/lib src/main.cpp -lsfml-graphics -lsfml-window -lsfml-system -lpng -o rays
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
-    # Run the program
     ./rays
 else
     echo "Compilation failed."
